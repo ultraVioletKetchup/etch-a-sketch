7003d341743f90fc5a4f6a631d9305db37d8e734
Manny <emmanuel.benard116@gmail.com>
2024-02-12 22:01:56 -0500

    Add hover event listeners, they are running but not producing the effect

--- script.js
+++ script.js
@@ -4,4 +4,18 @@
      const gridItem = document.createElement('div');
      gridItem.classList.add('grid-item');
      gridContainer.appendChild(gridItem);
+
+     // Add event listener for hover
+     gridItem.addEventListener('mouseover', function() {
+     this.classList.add('hovered'); // Add 'hovered' class
+     console.log("mouseover event listener");
+     });
+
+    // Add event listener for mouseout 
+    gridItem.addEventListener('mouseout', function() {
+        this.classList.remove('hovered'); // Remove 'hovered' class
+        console.log("mouseout event listener");
+    });
  }
+
+  
